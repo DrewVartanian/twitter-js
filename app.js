89d5cc1fa12c2c312c7ed54f774ae282eef8d22d
Karen Mao <karen.mao707@gmail.com>
2015-08-05 14:37:06 -0400

    render files complete

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 var express = require('express');
+var swig = require('swig');
 var app = express();
 
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views/');
+// var s = swig.renderFile(__dirname + '/views/index.html', {
+// 	title : "An Example",
+// 	people : [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]
+// });
+
+// var title = "An Example";
+// var person = {}
 app.use(function(req,res,next){
 	process.stdout.write(req.method.toUpperCase() + " " + req.path+" ");
+	
 	next();
 })
 
 
 app.get('/', function(req,res){
 	var status=200;
-	console.log(status);
-	res.status(status).send('GET request to homepage');
+	
+	//res.status(status).send('GET request to homepage');
 	//res.sendStatus
 	//res.status(200).send("200 good");
+	//res.status(status).send(s);
+		res.render('index', { title : "An Example",
+	 	people : [{name: "Gandalf"}, {name: "Frodo"}, {name: "Hermione"}]});
 
 });
 

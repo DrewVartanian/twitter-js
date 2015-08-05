4d5051e3d72849d63683c1eb6afaf510a254e497
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-08-05 17:34:21 -0400

    Completed forms and http requests

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var router = express.Router();
 
 var tweetBank = require('../tweetBank');
 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 router.get('/', function (req, res) {
   // res.sendFile(__dirname+"/public/stylesheets/style.css");
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
+});
+
+router.post('/submit', urlencodedParser, function (req, res) {
+  // res.sendFile(__dirname+"/public/stylesheets/style.css");
+  tweetBank.add(req.body.name,req.body.text);
+  res.redirect('/');
+});
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name, list: list, showForm: true, name: name } );
 });
 
 module.exports = router;
